c16b7c55a8eb1e53b6ecf15c0f810c3cebba60d3
shineun-kyu <shineunkue0804@gmail.com>
2023-03-27 18:04:48 +0900

    Add hw3.py

--- hw3.py
+++ hw3.py
@@ -1,7 +1,7 @@
 def get_fixed_price(t,a,b) :
-    print('A 상품의 정가는 ',a / (1 - t / 100),'원')
-    print('B 상품의 정가는 ',b / (1 - t / 100),'원')
+    print(b,'상품의 정가는 ',a / (1 - t / 100),'원')
 percent = int(input('할인율은? :'))         
 a = int(input('A 상품의 할인된 가격은은? :'))
 b = int(input('B 상품의 할인된 가격은은? :'))
-get_fixed_price(percent,a,b)
+get_fixed_price(percent,a,'A')
+get_fixed_price(percent,b,'B')
